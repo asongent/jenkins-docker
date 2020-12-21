e458a4423a26f8b640c9703be2644bac62f44ba5
Alfred. N <asongentle@gmail.com>
2020-12-21 17:35:42 -0500

    updates main

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello NodeApp!! \n");
+  response.end("Hello Guys I have successfully integrated my Github-Jenkins-Docker!! \n");
 });
 
 // listen on localhost:8000
 server.listen(8000);
 console.log("Server listening on port 8000 :  http://127.0.0.1:8000/");
 
-good
